b316798244fbc17965a1e30d816ea4cfa62291bf
rafidhaque <rafidhaque@iut-dhaka.edu>
2020-09-18 06:05:53 +0600

    Created a basic window with a button in the middle.

---
+++ JavaFX/src/Main.java
@@ -0,0 +1,32 @@
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+
+public class Main extends Application {
+
+    Button button;
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        // entire window is stage.
+        // inside the stage in scene, stuff that's inside.
+
+        primaryStage.setTitle("Title of my name");
+        button = new Button("Click me man");
+
+        // layout: how everything is staged.
+        StackPane layout = new StackPane(); // position button in the middle
+        layout.getChildren().add(button);
+
+        Scene scene = new Scene(layout, 300, 250);
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+}
